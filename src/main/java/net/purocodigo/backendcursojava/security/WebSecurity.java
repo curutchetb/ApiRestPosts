c667e190cb99820365cc21abd9de965947833732
curutchetb <curutchetb@gmail.com>
2022-08-19 16:19:03 -0300

    services update

--- src/main/java/net/purocodigo/backendcursojava/security/WebSecurity.java
+++ src/main/java/net/purocodigo/backendcursojava/security/WebSecurity.java
@@ -24,7 +24,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
         .antMatchers(HttpMethod.POST, "/users").permitAll()
         .antMatchers(HttpMethod.GET, "/posts/last").permitAll()
         .antMatchers(HttpMethod.GET, "/posts/{id}").permitAll()
